a205956ff816bdfa3943244db393fc2559f5d67f
MariKKKKKKK <mark.prnk@gmail.com>
2017-10-05 19:38:11 +0300

    Delete some useless methods.

--- src/app/cv.service.ts
+++ src/app/cv.service.ts
@@ -17,16 +17,7 @@ export class CvService {
   getCvData(): Promise<Cv> {
     return this.http.get(this.cvUrl).toPromise().then(response => response.json() as Cv ).catch(this.handleError);
   }
-  getBasicInfoData(): Promise<BasicInfo> {
-    return Promise.resolve(cvData.basicInfo);
-  }
-  getEducationData(): Promise<Education> {
-    return Promise.resolve(cvData.education);
-
-  }
-  getSkillsData(): Promise<string[]> {
-    return Promise.resolve(cvData.skills);
-  }
+ 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
